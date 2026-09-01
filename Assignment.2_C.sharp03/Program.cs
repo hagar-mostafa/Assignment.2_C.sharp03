f5f380dcdff42b2c4d98efed66abc5cf8dc5dd7c
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-01 15:11:40 +0200

    solve.Q1

--- Assignment.2_C.sharp03/Program.cs
+++ Assignment.2_C.sharp03/Program.cs
@@ -3,7 +3,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-       
+        //----------------Q1------------------------
+        int pages = 464;
+        bool isAvailable = true;
+        if (pages > 300 && isAvailable)
+        {
+            Console.WriteLine("You can borrow this book");
+        }
+
 
     }
 }
